86bd4d43f0d39c3c0b4e18386c50d7a7ca1ec540
Clinton431 <clintonnyakoe@gmail.com>
2024-01-02 15:53:15 +0300

    Exercise One

---
+++ 02_Day_Introduction_to_React/Learn.jsx
@@ -0,0 +1,10 @@
+const jsxElement = <h1>I am a JSX element</h1> //JSX with HTML
+const header = (
+    <header>
+        <h1>Welcome to 30 days of React</h1>
+        <h2>Getting started React</h2>
+        <h3>Javascript Library</h3>
+        <p>Clinton Nyakoe</p>
+        <small>Jan 3, 2024</small>
+    </header>
+)
